afec6fd1af46086d9220d48e27be6ec27f77628f
Obijuan <obijuan.cube@gmail.com>
2021-03-13 12:06:45 +0100

    Priting raw content

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Icestudio-cli
 Small utilities for working with .ice icestudio files in the command line
+
+
+## Test files
+
+The Icestudio files used for testing are located in the **Test-files** folder
+
+* **test-01-info.ice**: It only contains a basic info block
+
+## External node.js modules
+
+* [colors](https://nodejs.org/en/knowledge/command-line/how-to-get-colors-on-the-command-line/): Print colors on the command line
+
+## Quick start
+
+* Install all the packages: `npm install`
+* Run the test.js example: `node test.js`
+
+```
+----------------------------------------
+File: Test-files/test-01-info.ice
+----------------------------------------
+Raw content: 
+{ version: '1.2',
+  package:
+   { name: '', version: '', description: '', author: '', image: '' },
+  design:
+   { board: 'alhambra-ii', graph: { blocks: [Array], wires: [] } },
+  dependencies: {} }
+
+```
+
--- test.js
+++ test.js
@@ -1,5 +1,42 @@
+//-------------------------
+//-- Simple test file
+//-------------------------
+
+//-- fs module for accesing the filesystem
+const fs = require('fs');
+
+//-- Module for printing in colors on the console
 const colors = require('colors');
 
-console.log('This is a plain string');
-console.log('This string is blue'.blue);
+//-- Icestudio file to analyze
+const ICE_FILE = 'Test-files/test-01-info.ice'
+
+//-- Header length in characteres
+const HLEN = 40;
+
+console.log("");
+console.log("-".repeat(HLEN));
+console.log(`File: ${ICE_FILE}`);
+console.log("-".repeat(HLEN));
+
+//-- Icestudio file data
+let ice_data;
+
+//-- Read the Icestudio file
+try {
+  ice_data = fs.readFileSync(ICE_FILE);
+
+} catch (error) {
+  console.log(error.message.red + '\n');
+  process.exit(1);  
+}
+
+//-- The icestudio file is in JSON format
+//-- Parse it and get the root Object
+let ice = JSON.parse(ice_data);
+
+console.log("Raw content: ".blue);
+
+console.log(ice);
+console.log("");
 
