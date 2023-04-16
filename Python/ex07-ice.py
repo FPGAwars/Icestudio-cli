211405d680f7663722a7f644c840fb0d2e17b305
Obijuan <obijuan.cube@gmail.com>
2023-04-16 18:30:44 +0200

    Add more examples and test files

---
+++ Python/ex07-ice.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import json
+from Icestudio import Ice, Size, Blocks, Graph
+
+ice = Ice()
+ice.open_file("../Test-files/test-13-bit1-0.ice")
+
+#-- Print info about the circuit
+print(f"* Blocks: {len(ice.design.graph.blocks.list)}")
+print(f"* Wires: {len(ice.design.graph.wires)}")
+
+print()
+
+block = ice.design.graph.blocks.list[0]
+
+
+#-- Show all the blocks with its types and ids
+blocks = ice.design.graph.blocks.list
+for block in blocks:
+    ublock = ice.dependencies.dict[block.type]
+    print(f"-{ublock.package.name} (v{ublock.package.version})")
+
+print()
---
+++ Python/ex08-ice.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import sys
+from Icestudio import Ice
+
+def is_basic_block(type):
+    return type in ['basic.output', 'basic.info', 'basic.code', 'basic.input']
+
+
+#-- The first argument is the .ice circuit to analyze
+if ( len(sys.argv) < 2 ):
+    print("No icestudio file")
+    print("Usage: ex08-ice.py icestudio-file")
+    print()
+    sys.exit()
+
+file = sys.argv[1]
+
+#-- Read the file
+ice = Ice()
+ice.open_file(file)
+
+#-- Top level blocks
+blocks = ice.design.graph.blocks.list
+
+#-- List with basic blocks
+basic_lst = []  #-- Basic blocks
+block_lst = []  #-- User blocks
+
+#-- Fill the lists with the type of block
+for block in blocks:
+
+    if is_basic_block(block.type):
+        basic_lst.append(f"  -{block.type}")
+
+    else:
+        ublock = ice.dependencies.dict[block.type]
+        block_lst.append(f"  -{ublock.package.name}" 
+                         f" (v{ublock.package.version})")
+
+#-- Number of blocks
+nbasic = len(basic_lst)
+nblock = len(block_lst)
+
+#-- Print the blocks
+print(f"File: {file}")
+print()
+
+print(f"Number of blocks: {nbasic + nblock}\n")
+
+if nblock > 0:
+    print(f"* Blocks: {nblock}")
+    print("\n".join(block_lst))
+    print()
+
+if nbasic > 0:
+    print(f"* Basic blocks: {nbasic}")
+    print("\n".join(basic_lst))
+
+print()
